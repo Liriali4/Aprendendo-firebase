9a58f00cc55c2a56e7c367ecd781a0e999b268c5
Líria Li4 <lirialia51@gmail.com>
2023-02-24 14:26:25 +0100

    adicionado código que guarda os dados do input na base de dados

--- src/componentes/formulario/formulario.js
+++ src/componentes/formulario/formulario.js
@@ -1,17 +1,59 @@
+import { app } from "../../config/firebase";
+import { useState } from "react";
 import "./formulario.css"
+import { addDoc, collection, doc, getFirestore } from "firebase/firestore";
 
 export default function Formulario() {
+    
+    const [nome, setNome] = useState("");
+    const [id, setId] = useState("");
+
+    const bd = getFirestore(app);
+
+    async function add(e){
+        e.preventDefault()
+        try {
+            console.log("Aqui")
+            
+        await addDoc(collection(bd, "Filmes"), {
+            id: id,
+            name: nome,
+        });  
+        } catch (error) {
+            console.log("Erro" , error)
+        }
+    }
 
     return (
         <>
             <form>
                 <h3>Ver capa dos filmes</h3>
-                <label>Nome</label>
-                <input type="email" placeholder="insira o seu email"></input>
 
-                <label>ID</label>
-                <input type="text" placeholder="insira o seu ID"></input>
-                <button className="pesquisar">Pesquisar</button>
+                <label >Nome</label>
+                <input 
+                name="nome" 
+                id="nome"
+                type="text" 
+                placeholder="insira o seu email"
+                value={nome}
+                onChange={(event)=> setNome(event.target.value)}
+                />
+
+                <label >ID</label>
+                <input 
+                name="id" 
+                id="id" 
+                type="text" 
+                placeholder="insira o seu ID"
+                value={id}
+                onChange={(event)=> setId(event.target.value)}
+                />
+
+                <button 
+                className="pesquisar" 
+                onClick={add}>
+                    Pesquisar
+                </button>
             </form>
         </>
 
--- src/componentes/paraexibir/paraexibir.js
+++ src/componentes/paraexibir/paraexibir.js
@@ -1,6 +1,9 @@
 import "./paraexibir.css"
 import { app } from "../../config/firebase";
-import { getFirestore, Timestamp } from "firebase/firestore";
+
+import { getFirestore}
+ from "firebase/firestore";
+
 import { doc, setDoc } from "firebase/firestore";
 
 export default function ParaExibir(){
@@ -8,58 +11,17 @@ export default function ParaExibir(){
     const bd = getFirestore(app);
 
     async function adicionar(){
-        
-           /* Se o documento não existir, ele será criado. 
-            Se o documento existir, o conteúdo dele será 
-            substituído pelos dados recém-fornecidos como mostrado.*/
-
-
-          await setDoc(doc(bd, "Filmes", "1"), {
-        id: "1",
-        name: "Baby boss 1",
-        category: "animação",
-        classification:"+ 7",
-        description: "O primeiro filme"
-    
-          });   
-
-         /*    Se você não tiver certeza de que o 
-            documento existe, ignore a opção de 
-            mesclar novos dados com qualquer documento
-            atual para evitar a substituição de documentos inteiros.
-
-    const filmesref = doc(bd, 'Filmes', '1');
-    setDoc(filmesref, { capital: "O primeiro filme de dois" });
-
-            Criar coleção com diferentes tipoos de dados
-                
-    const filmesref = {
-        stringExample: "Hello world!",
-        booleanExample: true,
-        numberExample: 3.14159265,
-        dateExample: Timestamp.fromDate(new Date("December 10, 1815")),
-        arrayExample: [5, true, "hello"],
-        nullExample: null,
-        objectExample: {
-            a: 5,
-            b: {
-                nested: "foo"
-            }
-        }
-    };
-    await setDoc(doc(bd, "Filmes", "one"), filmesref);
-
-*/
-
-
-
+        await setDoc(doc(bd, "Filmes", "1"), {
+            id: "2",
+            name: "nome",
+        }); 
 }
   
 
     return(
         <div className="paraexibir">
 
-        <button onClick={adicionar}>Criar coleção</button>
+        <button onClick={ adicionar }>Criar coleção</button>
         </div>        
     );
 }
