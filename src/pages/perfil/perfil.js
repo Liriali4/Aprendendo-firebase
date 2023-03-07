2c1983573b3cc0e4381f7750d2ae58954c4d7d83
Líria Li4 <lirialia51@gmail.com>
2023-03-07 17:43:31 +0100

    está funcional, guarda no Storage e exibe na tela

--- src/pages/perfil/perfil.js
+++ src/pages/perfil/perfil.js
@@ -1,40 +1,61 @@
 import { useState } from "react"
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import "./perfil.css"
 
-export default function Perfil(){
+export default function Perfil() {
     const [imgUrl, setImgUrl] = useState();
+    const [photoView, setPhotoView] = useState('');
+    const [photoProfile, setPhotoProfile] = useState();
 
-    function pegarfoto(event){
+    function pegarfoto(event) {
         event.preventDefault()
-        
+
         const storage = getStorage();
-        const storageRef = ref(storage, `imagens/${imgUrl.name}`);
+        const storageRef = ref(storage, `imagens/${photoProfile.name}`);
 
-        
-        const metadata = {
-        contentType: 'image/jpeg',
-        };
-
+        const uploadTask = uploadBytesResumable(storageRef, photoProfile);
       
-        const uploadTask = uploadBytes(storageRef, imgUrl, metadata);
-        console.log(uploadTask)
+        getDownloadURL(uploadTask.snapshot.ref)
+        .then(url => {
+            console.log(url)
+        })
+
     }
 
-    return(
+    const previewPhotoLoaded = (e) => {
+
+        try {
+            const reader = new FileReader();
+            const filePhoto = e.target.files;
+
+            reader.onload = (e) => {
+                setPhotoView(e.target?.result);
+            };
+            reader.readAsDataURL(filePhoto[0]);
+                setPhotoProfile(filePhoto[0]);
+        } catch (error) {
+            setPhotoView('');
+        }
+    };
+
+    return (
         <main>
             <div className="imagem">
-               {imgUrl && <img src={imgUrl} alt="imagem" />}
+                <img src={photoView?photoView:'https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg?20090511140841'} alt="imagem" />
             </div>
             <div className="pegarimg">
                 <form className="formperfil"  >
-                    <input 
-                    type="file"
-                    value={imgUrl}
-                    onChange={(event)=> setImgUrl(event.target.value)}
+                    <input
+                        type="file"
+                        value={imgUrl}
+                        onChange={(e) => previewPhotoLoaded(e)}
                     />
-                    <button className="carregarimg" onClick={pegarfoto} >Carregar imagem</button>
+                    <button
+                        className="carregarimg"
+                        type="submit"
+                        onClick={(e) =>
+                            pegarfoto(e)
+                        } >Carregar imagem</button>
                 </form>
             </div>
         </main>
